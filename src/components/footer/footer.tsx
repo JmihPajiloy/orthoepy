09185f77bd8cfa902237ecec71a22904b0b67f41
JmihPajiloy <svanyokshib@gmail.com>
2024-03-21 03:28:49 +0500

    fixed ugly footer with open accordion

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,11 +5,11 @@ import { Link } from "react-router-dom";
 export const Footer = () => {
   return (
 
-    <div>
+    <div className="mt-5">
       <Separator orientation="horizontal" />
-      <div className="flex justify-center pb-5 px-2">
+      <div className="flex justify-center pb-5 px-3">
         <div className="screen-wide flex justify-between">
-          <Button variant="link" className="text-muted-foreground">
+          <Button variant="link" className="text-muted-foreground pl-0">
             <Link to="https://github.com/JmihPajiloy/orthoepy">
               <GitHubLogoIcon className="h-5 w-5 mr-2 " />
             </Link>Github
