2195ef0520d70b3029a3e2d95fadb523c84c4361
JmihPajiloy <svanyokshib@gmail.com>
2024-03-30 13:18:30 +0500

    changed word-list icon

--- src/components/header/to-word-list.tsx
+++ src/components/header/to-word-list.tsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
-import { ReaderIcon } from "@radix-ui/react-icons";
+import { LetterCaseCapitalizeIcon } from "@radix-ui/react-icons";
 import { Button } from "@shadcn/ui";
 
 export const ToWordList = () => {
   return (
     <Button size="icon" variant="ghost" className="mobile">
       <Link to="/word-list">
-        <ReaderIcon className="h-5 w-5" />
+        <LetterCaseCapitalizeIcon className="h-5 w-5" />
       </Link>
     </Button>
   );
